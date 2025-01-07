47b865c7e1ba6057bff17b561c81aeeb5a586407
LennyTaelman <lenny.taelman@gmail.com>
2025-01-07 16:27:19 +0100

    bug in tauto?

---
+++ Setauto/Bug.lean
@@ -0,0 +1,7 @@
+import Mathlib.Tactic.Tauto
+
+
+lemma weird (α : Type) (h : α → False) (x : α) : False := by
+  tauto -- No goals, yet lean gives "application type mismatch"
+
+#check weird -- unknown identifier 'weird'
