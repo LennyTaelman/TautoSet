d2c90ba111ab3dc414e6ac17f1f65093cd6ecb16
LennyTaelman <lenny.taelman@gmail.com>
2025-01-10 16:14:20 +0100

    no need for bug.lean anymore

--- Setauto/Bug.lean
+++
@@ -1,8 +0,0 @@
-import Mathlib.Tactic.Tauto
-
--- solved with Mathlib bump
-
-lemma weird (α : Type) (h : α → False) (x : α) : False := by
-  tauto -- No goals, yet lean gives "application type mismatch"
-
-#check weird -- unknown identifier 'weird'
